24b20fe59f506a88773ab3930a87151320ca7a26
Hazem Aamer <zicoaamer@gmail.com>
2025-04-06 01:43:13 +0200

    Fixing vercel error 500

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
