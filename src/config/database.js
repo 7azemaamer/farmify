33c1665c61415f8c9fa8893a017bf5793f4659e0
Hazem Aamer <zicoaamer@gmail.com>
2025-04-07 23:06:19 +0200

    Added ENV guide

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
-
+//Add connection string for mongodb deployment
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
